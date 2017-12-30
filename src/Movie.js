57acd8ff32df525d4f4e2ab2ed86ebb18728434a
KunhoLee <gunho1020@gmail.com>
2017-12-30 12:16:47 +0900

    Movie 컴포넌트가 render할 JSX 작성

--- src/Movie.js
+++ src/Movie.js
@@ -3,6 +3,19 @@ import React from 'react';
 function Movie({title, poster, genres, synopsis}) {
   return (
     <div className="Movie">
+      <div className="Movie__Column">
+        <img src={poster} alt={title} />
+      </div>
+      <div className="Movie__Column">
+        <h1>{title}</h1>
+        <div className="Movie__Genres">
+          <span className="Movie__Genre">{genres[0]}</span>
+          <span className="Movie__Genre">{genres[1]}</span>
+        </div>
+        <p className="Movie__Synopsis">
+          {synopsis}
+        </p>
+      </div>
     </div>
   )
 }

ac77bbb9d4586666a0843abf696a0428ae5fe434
KunhoLee <gunho1020@gmail.com>
2017-12-30 20:31:00 +0900

    Movie 컴포넌트에서 synopsis를 출력하는 p 태그를 react-lines-ellipsis 패키지로 변경

--- src/Movie.js
+++ src/Movie.js
@@ -15,9 +15,15 @@ function Movie({title, poster, genres, synopsis}) {
         <div className="Movie__Genres">
           {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
         </div>
-        <p className="Movie__Synopsis">
-          {synopsis}
-        </p>
+        <div className="Movie__Synopsis">
+          <LinesEllipsis
+            text={synopsis}
+            maxLine='3'
+            ellipsis='...'
+            trimRight
+            basedOn='letters'
+          />
+        </div>
       </div>
     </div>
   )

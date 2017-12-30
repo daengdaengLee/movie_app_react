16250edc8134dc73eff1629bb4d3b005de51c0aa
KunhoLee <gunho1020@gmail.com>
2017-12-30 20:30:05 +0900

    Movie 컴포넌트에 react-lines-ellipsis 패키지 import

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
 import MoviePoster from './MoviePoster.js';
 import MovieGenre from './MovieGenre.js';
 

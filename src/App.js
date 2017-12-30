94268aa073c5c30a9797105e3e8ff7bc9ef95912
KunhoLee <gunho1020@gmail.com>
2017-12-30 15:06:48 +0900

    App 컴포넌트에 Movie 컴포넌트 주입

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Movie from './Movie.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hi!
+        <Movie
+          title="Movie test"
+          poster="https://source.superherostuff.com/wp-content/uploads/2017/05/7773769005_star-wars.jpg"
+          genres={['SF', 'Action']}
+          synopsis="Jedi vs Sith"
+        />
       </div>
     );
   }
